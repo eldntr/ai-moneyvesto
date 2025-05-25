6797ec093088ce2c2fdc5f690f071db741d70f02
Eldintaro Farrandi <5025221299@student.its.ac.id>
2025-05-25 12:42:46 +0700

    add cors

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,11 @@
 # app/__init__.py
 from flask import Flask
+from flask_cors import CORS
 
 def create_app():
     """Application factory function."""
     app = Flask(__name__)
+    CORS(app)  
 
     # Impor dan daftarkan blueprint
     from .routes import chat_bp
